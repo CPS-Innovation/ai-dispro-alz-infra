2c01bcf9f50181d25705c9a23e1ec15c2a35620d
Narayn Manoharan <Narayn.Manoharan@cps.gov.uk>
2026-01-21 12:22:11 +0000

    Adding AI Services

---
+++ terraform/azure-workloads/code/documentIntelligence.tf
@@ -0,0 +1,24 @@
+resource "azurerm_cognitive_account" "document_intelligence" {
+  name                = "di-aid-${var.environment}-01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  kind     = "FormRecognizer"
+  sku_name = "S0"
+
+  custom_subdomain_name = "di-aid-${var.environment}-01"
+
+  # Network access
+  public_network_access_enabled = false
+
+  network_acls {
+    default_action = "Allow"
+    ip_rules       = []
+  }
+
+  local_auth_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
